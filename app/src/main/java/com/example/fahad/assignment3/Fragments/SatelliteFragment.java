7d0ed86611b40919d33ff1381908a9f7be450170
fur866 <fur866@gmail.com>
2016-05-23 11:39:33 +1000

    Step 2: Properties in satellite fragment have been added.

--- app/src/main/java/com/example/fahad/assignment3/Fragments/SatelliteFragment.java
+++ app/src/main/java/com/example/fahad/assignment3/Fragments/SatelliteFragment.java
@@ -13,10 +13,46 @@ import com.example.fahad.assignment3.R;
  */
 public class SatelliteFragment extends Fragment {
 
+    private String latitude;
+    private String longitude;
+    private final String api_key = getResources().getString(R.string.api_key);
+    private final String api_endPoint = getResources().getString(R.string.api_endPoint);
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
     {
         View view = inflater.inflate(R.layout.satellite_fragment_view, container,false);
         return view;
+    }
+
+    public void setLatitude(String latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude)
+    {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude()
+    {
+        return this.latitude;
+    }
+
+    public String getLongitude()
+    {
+        return this.longitude;
+    }
+
+    public String getApiKey()
+    {
+        return this.api_key;
+    }
+
+    public  String getApiEndPoint()
+    {
+        return this.api_endPoint;
     }
 }
